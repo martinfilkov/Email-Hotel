533c209d7de2e7015ebd0defd3dff699ad1925b3
martinfilkov <martinfilkov1@gmail.com>
2024-08-15 19:00:52 +0300

    Initial commit

---
+++ rest/src/main/java/com/tinqinacademy/email/rest/EmailApplication.java
@@ -0,0 +1,13 @@
+package com.tinqinacademy.email.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.tinqinacademy.email")
+public class EmailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
+}
